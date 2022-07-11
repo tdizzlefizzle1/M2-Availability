0e8bc39b75eeaa96f83b168e686936c4fd89f91c
tdizzlefizzle1 <tylerhols@yahoo.com>
2022-07-11 22:20:59 +0000

    Fixed text input validation so there can be spaces in the text file

--- sender.sh
+++ sender.sh
@@ -7,39 +7,39 @@ while read -r line; do
 	
 	if [[ -n $line ]]; then
 		if [[ $line == *"smtp"* ]]; then
-			smtp=$(echo "$line" | awk -F "=" '{ print $2 }')
+			smtp=$(echo "$line" | awk -F "=" '{ print $2 }' | xargs)
 			printf "smtp: $smtp\n"
 			#send_email="$send_email '$smtp"
 
 		elif [[ $line == *"receiver"* ]]; then
-			rec=$(echo "$line" | awk -F "=" '{ print $2 }')
+			rec=$(echo "$line" | awk -F "=" '{ print $2 }' | xargs)
 			printf "receiver: $rec\n"
 			mail_rcpt="$mail_rcpt --mail-rcpt ${rec}"
 
 		elif [[ $line == *"port"* ]]; then
-			port=$(echo "$line" | awk -F "=" '{ print $2 }')
+			port=$(echo "$line" | awk -F "=" '{ print $2 }' | xargs)
 			printf "port: $port\n"
 			#send_email="$send_email:$port'"
 
 		elif [[ $line == *"sender"* ]]; then
-			sender=$(echo "$line" | awk -F "=" '{ print $2 }')
+			sender=$(echo "$line" | awk -F "=" '{ print $2 }' | xargs)
 			printf "sender: $sender\n"
 			#send_email="$send_email"
 		
 		elif [[ $line == *"password"* ]]; then
-			password=$(echo "$line" | awk -F "=" '{ print $2 }')
+			password=$(echo "$line" | awk -F "=" '{ print $2 }' | xargs)
 			printf "sender password: $password\n"
 		
 		elif [[ $line == *"from"* ]]; then
-			from=$(echo "$line" | awk -F "=" '{ print $2 }')
+			from=$(echo "$line" | awk -F "=" '{ print $2 }' | xargs)
 			printf "from: $from\n"
 		fi	
 	fi
 done <$file
+echo ""
+echo ""
+#printf "$smtp\n$rec\n$port\n$sender\n$password\n$from\n"
 
-printf "$mail_rcpt\n"
-echo ""
-echo ""
 curl \
 	--ssl-reqd --url "smtp://${smtp}:${port}" \
  	--user "${sender}:${password}" \
